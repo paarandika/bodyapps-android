af0ee7a98ed862e048d905d333a3979e9120386b
paarandika <paarandika@gmail.com>
2014-05-25 00:52:50 +0530

    Finished saved items list base UI

--- gen/fossasia/valentina/bodyapp/main/R.java
+++ gen/fossasia/valentina/bodyapp/main/R.java
@@ -1001,7 +1001,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050048;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1009,6 +1009,7 @@ containing a value of this type.
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f050042;
+        public static final int container2=0x7f050045;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1018,6 +1019,7 @@ containing a value of this type.
         public static final int exit=0x7f050041;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int fragment1=0x7f050043;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1025,8 +1027,9 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int imageView1=0x7f05003c;
         public static final int listMode=0x7f050001;
-        public static final int listView1=0x7f050043;
+        public static final int listView1=0x7f050046;
         public static final int list_item=0x7f050029;
+        public static final int mesurements=0x7f050044;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1052,11 +1055,12 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
-        public static final int svd_date=0x7f050045;
-        public static final int svd_image=0x7f050047;
-        public static final int svd_name=0x7f050044;
-        public static final int svd_sync=0x7f050046;
+        public static final int svd_date=0x7f050049;
+        public static final int svd_image=0x7f05004b;
+        public static final int svd_name=0x7f050048;
+        public static final int svd_sync=0x7f05004a;
         public static final int tabMode=0x7f050002;
+        public static final int tex=0x7f050047;
         public static final int textView1=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
@@ -1115,14 +1119,17 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int activity_saved=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int fragment_saved=0x7f03001a;
-        public static final int saved_tab=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_view_saved=0x7f030019;
+        public static final int fragment_main=0x7f03001a;
+        public static final int fragment_saved=0x7f03001b;
+        public static final int fragment_view_saved=0x7f03001c;
+        public static final int saved_tab=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
         public static final int saved=0x7f0c0001;
+        public static final int view_saved=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1169,6 +1176,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_saved=0x7f0a0010;
+        public static final int title_activity_view_saved=0x7f0a0011;
     }
     public static final class style {
         /** 
