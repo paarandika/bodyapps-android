d8a6f779f6c6bc3caa47d5936564fba4adabd866
paarandika <paarandika@gmail.com>
2014-05-29 00:54:15 +0530

    Removed support libraries for action bar and fragment. made the min sdk v.11

--- src/fossasia/valentina/bodyapp/main/MainActivity.java
+++ src/fossasia/valentina/bodyapp/main/MainActivity.java
@@ -1,20 +1,15 @@
 package fossasia.valentina.bodyapp.main;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	private Button create;
 	private Button saved;
